1e11b0d7b8e5871662f3d5091abcb280356ade71
Tim <aboutim2020@gmail.com>
2024-06-19 01:47:41 -0700

    add threadNum = 0

--- nachos-4.0-final/code/userprog/userkernel.cc
+++ nachos-4.0-final/code/userprog/userkernel.cc
@@ -27,6 +27,7 @@ UserProgKernel::UserProgKernel(int argc, char **argv)
     for (int i = 0; i < NumPhysPages; i++)
         PhysicalPageUsed[NumPhysPages] = FALSE;
 	execfileNum = 0;
+    threadNum = 0;
     for (int i = 1; i < argc; i++) {
         if (strcmp(argv[i], "-s") == 0) {
 		    debugUserProg = TRUE;
