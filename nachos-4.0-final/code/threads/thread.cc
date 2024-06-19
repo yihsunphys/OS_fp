820512cf7f7f28dda12f90f935dc7062aa927d5e
Tim <aboutim2020@gmail.com>
2024-06-19 07:41:16 -0700

    update sleep function

--- nachos-4.0-final/code/threads/thread.cc
+++ nachos-4.0-final/code/threads/thread.cc
@@ -268,9 +268,9 @@ Thread::Sleep (bool finishing)
     //cout << "Sleeping thread: " << name << ", ID: " << ID;
     status = BLOCKED;
 
-    if(getRemainingBurstTime()- RunTime > 0 && RunTime!=0){
+    if(RunTime!=0){
         DEBUG('z', "[UpdateRemainingBurstTime] Tick ["<<kernel->stats->totalTicks<<"]: Thread ["<<getID()<<"] update remaining burst time, from: [" << RemainingBurstTime << "] - [" << min(RunTime,RemainingBurstTime) << "], to [" << max(0, RemainingBurstTime - RunTime) <<"]");
-        RemainingBurstTime = max(0, RemainingBurstTime - kernel->scheduler->RunTime());
+        RemainingBurstTime = max(0, RemainingBurstTime - RunTime);
     }
 
     while ((nextThread = kernel->scheduler->FindNextToRun()) == NULL)
